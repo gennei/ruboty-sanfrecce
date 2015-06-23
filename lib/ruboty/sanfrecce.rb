ab943aa3f8e5e935bc6a0a43133ef40b167a210c
gennei <sai.gennei@gmail.com>
2015-06-24 00:13:12 +0900

    first commit

---
+++ lib/ruboty/sanfrecce.rb
@@ -0,0 +1,8 @@
+require "ruboty/sanfrecce/version"
+require "ruboty/handlers/sanfrecce"
+
+module Ruboty
+  module Sanfrecce
+    # Your code goes here...
+  end
+end
---
+++ lib/ruboty/sanfrecce/version.rb
@@ -0,0 +1,5 @@
+module Ruboty
+  module Sanfrecce
+    VERSION = "0.1.0"
+  end
+end
